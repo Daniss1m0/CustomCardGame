b1fbfed348f722378405c9399eace3279c473b6b
Daniil <daniildu437@gmail.com>
2025-03-29 17:28:19 +0100

    Added enemyfield

--- Assets/Scripts/CardMovementScr.cs
+++ Assets/Scripts/CardMovementScr.cs
@@ -9,6 +9,7 @@ public class CardMovementScr : MonoBehaviour, IBeginDragHandler, IDragHandler, I
     private Vector3 offset;
     public Transform DefaultParent, DefaultTempCardParent;
     private GameObject TempCardGO;
+    public bool IsDraggable;
 
     void Awake()
     {
@@ -22,6 +23,11 @@ public class CardMovementScr : MonoBehaviour, IBeginDragHandler, IDragHandler, I
 
         DefaultParent = DefaultTempCardParent = transform.parent;
 
+        IsDraggable = DefaultParent.GetComponent<DropPlaceScr>().Type == FieldType.SELF_HAND;
+
+        if (!IsDraggable)
+            return;
+
         TempCardGO.transform.SetParent(DefaultParent);
         TempCardGO.transform.SetSiblingIndex(transform.GetSiblingIndex());
 
@@ -31,6 +37,9 @@ public class CardMovementScr : MonoBehaviour, IBeginDragHandler, IDragHandler, I
 
     public void OnDrag(PointerEventData eventData)
     {
+        if (!IsDraggable)
+            return;
+
         Vector3 newPos = MainCamera.ScreenToWorldPoint(eventData.position);
         transform.position = newPos + offset;
 
@@ -42,6 +51,10 @@ public class CardMovementScr : MonoBehaviour, IBeginDragHandler, IDragHandler, I
 
     public void OnEndDrag(PointerEventData eventData)
     {
+
+        if (!IsDraggable)
+            return;
+
         transform.SetParent(DefaultParent);
         GetComponent<CanvasGroup>().blocksRaycasts = true;
 

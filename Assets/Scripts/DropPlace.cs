bdbb2e9b27ac56f5fb8ca3abc7c40de37d125a5f
Daniil <daniildu437@gmail.com>
2025-03-29 11:36:42 +0100

    Added drag&drop system

---
+++ Assets/Scripts/DropPlace.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropPlace : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        Card card = eventData.pointerDrag.GetComponent<Card>();
+        if (card)
+            card.DefaultParent = transform;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null)
+            return;
+
+        Card card = eventData.pointerDrag.GetComponent<Card>();
+        if (card)
+            card.DefaultTempCardParent = transform;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null)
+            return;
+
+        Card card = eventData.pointerDrag.GetComponent<Card>();
+        if (card && card.DefaultTempCardParent == transform)
+            card.DefaultTempCardParent = card.DefaultParent;
+    }
+}

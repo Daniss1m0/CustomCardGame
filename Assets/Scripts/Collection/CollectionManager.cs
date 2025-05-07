cbdb7febf4a7c082f1285da0f72b78bec92f6d5c
Daniil <daniildu437@gmail.com>
2025-05-07 15:43:29 +0200

    Back-end for collection completed

--- Assets/Scripts/Collection/CollectionManager.cs
+++ Assets/Scripts/Collection/CollectionManager.cs
@@ -6,18 +6,63 @@ public class CollectionManager : MonoBehaviour
 {
     public GameObject CardPrefab;
     public Transform CardGrid;
+    public int CardsPerPage = 12;
+    public List<Button> PageButtons;
+
+    private List<Card> allCards;
+    private int currentPage = 0;
+    private List<GameObject> currentCardObjects = new List<GameObject>();
 
     void Start()
     {
-        LoadCards();
+        allCards = CardManager.AllCards;
+        ShowPage(0);
+        SetupButtons();
     }
 
-    void LoadCards()
+    void SetupButtons()
     {
-        foreach (Card card in CardManager.AllCards)
+        for (int i = 0; i < PageButtons.Count; i++)
+        {
+            int pageIndex = i;
+            PageButtons[i].onClick.AddListener(() => ShowPage(pageIndex));
+        }
+    }
+
+    void ShowPage(int pageIndex)
+    {
+        currentPage = pageIndex;
+
+        foreach (GameObject go in currentCardObjects)
+        {
+            Destroy(go);
+        }
+        currentCardObjects.Clear();
+
+        int start = pageIndex * CardsPerPage;
+        int end = Mathf.Min(start + CardsPerPage, allCards.Count);
+
+        for (int i = start; i < end; i++)
         {
             GameObject cardGO = Instantiate(CardPrefab, CardGrid);
-            SetupCardUI(cardGO, card);
+            SetupCardUI(cardGO, allCards[i]);
+            currentCardObjects.Add(cardGO);
+        }
+
+        UpdatePageButtons();
+    }
+
+    void UpdatePageButtons()
+    {
+        for (int i = 0; i < PageButtons.Count; i++)
+        {
+            var canvasGroup = PageButtons[i].GetComponent<CanvasGroup>();
+            if (canvasGroup == null)
+            {
+                canvasGroup = PageButtons[i].gameObject.AddComponent<CanvasGroup>();
+            }
+
+            canvasGroup.alpha = (i == currentPage) ? 1f : 0.5f;
         }
     }
 
@@ -27,7 +72,7 @@ public class CollectionManager : MonoBehaviour
         cardInfo.SetName(card.Name);
         cardInfo.SetLogo(card.Logo);
         cardInfo.SetStats(card.Attack, card.Health, card.Manacost);
-       
+
         Destroy(cardGO.GetComponent<CardMovement>());
         Destroy(cardGO.GetComponent<CardAbility>());
     }

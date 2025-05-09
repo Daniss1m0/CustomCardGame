f35f7e21e2afaefa0d5749c8194f1505779866c5
Daniil <daniildu437@gmail.com>
2025-05-09 12:24:50 +0200

    Completed back-end for DeckSelection scene

--- Assets/Scripts/DeckSelection/DeckSelectionUI.cs
+++ Assets/Scripts/DeckSelection/DeckSelectionUI.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI; // Не забудь!
 
 public class DeckSelectionUI : MonoBehaviour
 {
     public GameObject optionsPanel;
+    public Image heroImage; // Картинка справа
+    public Sprite[] deckSprites; // Сюда добавь спрайты для разных колод
+
+    private int currentDeckIndex = 0;
 
     public void OnPlayButton()
     {
@@ -17,6 +22,15 @@ public class DeckSelectionUI : MonoBehaviour
 
     public void LoadMainMenu()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void SelectDeck(int index)
+    {
+        if (index >= 0 && index < deckSprites.Length)
+        {
+            currentDeckIndex = index;
+            heroImage.sprite = deckSprites[index];
+        }
     }
 }

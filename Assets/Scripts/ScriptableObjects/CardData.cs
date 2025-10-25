9f3f362f61e6930ddee21e8b6f0bebb5bd81dab2
Daniil <daniildu437@gmail.com>
2025-10-25 16:12:17 +0200

    Add CardData ScriptableObject

---
+++ Assets/Scripts/ScriptableObjects/CardData.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Cards/CardData")]
+public class CardData : ScriptableObject
+{
+    [Header("Basic")]
+    public string cardName;
+    public int manaCost;
+    public Sprite logo;
+    public bool isSpell = false;
+
+    [Header("Stats (for non-spell)")]
+    public int attack;
+    public int health;
+
+    [Header("Abilities")]
+    public List<Card.AbilityType> abilities = new List<Card.AbilityType>();
+
+    [Header("Spell (if isSpell)")]
+    public SpellCard.SpellType spellType = SpellCard.SpellType.None;
+    public SpellCard.TargetType spellTarget = SpellCard.TargetType.None;
+    public int spellValue = 0;
+}

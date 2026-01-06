75ddf0e70d3279bd9a77ffc1f72d3acb069e1eda
Daniil <daniildu437@gmail.com>
2026-01-06 12:55:42 +0100

    Sync hero selection

--- Assets/Scripts/HeroSelectionUI.cs
+++ Assets/Scripts/HeroSelectionUI.cs
@@ -7,8 +7,19 @@ public class HeroSelectionUI : MonoBehaviour
     public Image heroImage;
     public Sprite[] heroSprites;
 
+    private int currentHeroIndex = 0;
+
+    private void Start()
+    {
+        currentHeroIndex = PlayerPrefs.GetInt("SelectedHeroIndex", 0);
+        UpdateVisuals();
+    }
+
     public void OnPlayButton()
     {
+        PlayerPrefs.SetInt("SelectedHeroIndex", currentHeroIndex);
+        PlayerPrefs.Save();
+
         SceneManager.LoadScene("MainScene");
     }
 
@@ -17,9 +28,18 @@ public class HeroSelectionUI : MonoBehaviour
         SceneManager.LoadScene("MainMenu");
     }
 
-    public void SelectDeck(int index)
+    public void SelectHero(int index)
     {
         if (index >= 0 && index < heroSprites.Length)
-            heroImage.sprite = heroSprites[index];
+        {
+            currentHeroIndex = index;
+            UpdateVisuals();
+        }
+    }
+
+    private void UpdateVisuals()
+    {
+        if (heroImage != null && heroSprites.Length > 0)
+            heroImage.sprite = heroSprites[currentHeroIndex];
     }
 }
--- Assets/Scripts/MainScene/HeroVisualSync.cs
+++ Assets/Scripts/MainScene/HeroVisualSync.cs
@@ -4,31 +4,72 @@ using Unity.Netcode;
 
 public class HeroVisualSync : NetworkBehaviour
 {
-    public Sprite hostAvatarSprite, clientAvatarSprite;
+    public Sprite[] allHeroSprites;
     public Image bottomHeroImage, topHeroImage;
+    public NetworkVariable<int> hostHeroIndex = new(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+    public NetworkVariable<int> clientHeroIndex = new(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
 
     public override void OnNetworkSpawn()
     {
-        UpdateVisuals();
+        hostHeroIndex.OnValueChanged += (oldV, newV) => UpdateUI();
+        clientHeroIndex.OnValueChanged += (oldV, newV) => UpdateUI();
+
+        int mySavedIndex = PlayerPrefs.GetInt("SelectedHeroIndex", 0);
+
+        SubmitHeroIndexServerRpc(mySavedIndex);
+
+        UpdateUI();
     }
 
-    private void UpdateVisuals()
+    public override void OnNetworkDespawn()
     {
+        hostHeroIndex.OnValueChanged -= (oldV, newV) => UpdateUI();
+        clientHeroIndex.OnValueChanged -= (oldV, newV) => UpdateUI();
+        base.OnNetworkDespawn();
+    }
+
+    [Rpc(SendTo.Server, InvokePermission = RpcInvokePermission.Everyone)]
+    private void SubmitHeroIndexServerRpc(int index, RpcParams rpcParams = default)
+    {
+        ulong senderId = rpcParams.Receive.SenderClientId;
+
+        if (senderId == NetworkManager.ServerClientId)
+            hostHeroIndex.Value = index;
+        else
+            clientHeroIndex.Value = index;
+    }
+
+    private void UpdateUI()
+    {
+        if (allHeroSprites == null || allHeroSprites.Length == 0) 
+            return;
+
+        Sprite hostSprite = GetSpriteSafe(hostHeroIndex.Value);
+        Sprite clientSprite = GetSpriteSafe(clientHeroIndex.Value);
+
         if (IsServer)
         {
             if (bottomHeroImage) 
-                bottomHeroImage.sprite = hostAvatarSprite;
+                bottomHeroImage.sprite = hostSprite;
 
             if (topHeroImage) 
-                topHeroImage.sprite = clientAvatarSprite;
+                topHeroImage.sprite = clientSprite;
         }
         else
         {
             if (bottomHeroImage) 
-                bottomHeroImage.sprite = clientAvatarSprite;
+                bottomHeroImage.sprite = clientSprite;
 
             if (topHeroImage) 
-                topHeroImage.sprite = hostAvatarSprite;
+                topHeroImage.sprite = hostSprite;
         }
+    }
+
+    private Sprite GetSpriteSafe(int index)
+    {
+        if (index >= 0 && index < allHeroSprites.Length)
+            return allHeroSprites[index];
+
+        return allHeroSprites.Length > 0 ? allHeroSprites[0] : null;
     }
 }
